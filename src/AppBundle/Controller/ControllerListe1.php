ffe8f91b86e4251beb46660471f12ffc70ac1e33
Justine Mignot <mignot.justine@gmail.com>
2016-10-29 23:32:28 +0100

    formulaire fonctionne, ajout d'une tache avec texte souhaité

--- src/AppBundle/Controller/ControllerListe1.php
+++ src/AppBundle/Controller/ControllerListe1.php
@@ -7,9 +7,48 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use AppBundle\Entity\Task;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class ControllerListe1 extends Controller
 {
+  /**
+  * @Route("/ajout", name="ajouterAction")
+  */
+  public function ajouterAction(Request $request)
+    {
+        // createFormBuilder is a shortcut to get the "form factory"
+        // and then call "createBuilder()" on it
+
+        $form = $this->createFormBuilder()
+            ->add('task', TextType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+       if ($form->isValid()) {
+           $data = $form->getData();
+
+           // ... perform some action, such as saving the data to the database
+             $task = new Task();
+             $task->setName($data["task"]);
+
+             $em = $this->getDoctrine()->getManager();
+
+             // tells Doctrine you want to (eventually) save the Task (no queries yet)
+             $em->persist($task);
+
+             // actually executes the queries (i.e. the INSERT query)
+             $em->flush();
+
+           return new Response('Saved new task with id '.$task->getId());
+       }
+
+        return $this->render('tests/ajout.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+
+
      /**
      * @Route("/liste", name="listeAction")
      */
@@ -30,23 +69,23 @@ class ControllerListe1 extends Controller
       //  var_dump($task); die;
     }
 
-    /**
-    * @Route("/ajout", name="createAction")
-    */
-    public function createAction()
-    {
-      $task = new Task();
-      $task->setName('Dire merci à Kévin');
-
-      $em = $this->getDoctrine()->getManager();
-
-      // tells Doctrine you want to (eventually) save the Task (no queries yet)
-      $em->persist($task);
-
-      // actually executes the queries (i.e. the INSERT query)
-      $em->flush();
-
-      return new Response('Saved new task with id '.$task->getId());
-    }
+    // /**
+    // * @Route("/ajout", name="createAction")
+    // */
+    // public function createAction()
+    // {
+    //   $task = new Task();
+    //   $task->setName('Dire merci à Kévin');
+    //
+    //   $em = $this->getDoctrine()->getManager();
+    //
+    //   // tells Doctrine you want to (eventually) save the Task (no queries yet)
+    //   $em->persist($task);
+    //
+    //   // actually executes the queries (i.e. the INSERT query)
+    //   $em->flush();
+    //
+    //   return new Response('Saved new task with id '.$task->getId());
+    // }
 
 }
